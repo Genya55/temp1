7b1bf67e33775cbce724747400fe4344b48b1047
Genya55 <nk12mv11033@gmail.com>
2024-04-09 22:57:20 -0500

    mod2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello React app with AWS</h2>
-        
+        <h3>I will stream temp data here</h3>
       </header>
     </div>
   );
